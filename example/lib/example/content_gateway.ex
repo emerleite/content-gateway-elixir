8ea802a54c44f8d103abd361bc406b63a20df9af
Emerson Macedo <emerleite@gmail.com>
2016-11-01 09:41:30 -0200

    Basic example

--- example/lib/example/content_gateway.ex
+++ example/lib/example/content_gateway.ex
@@ -1,3 +1,16 @@
 defmodule ContentGateway.Example do
   use ContentGateway
+
+  def connection_timeout do
+    2_000
+  end
+
+  def request_timeout do
+    1_000
+  end
+
+  def user_agent do
+    "Elixir (Example)"
+  end
+
 end
