b7da854625a59e9a8d24ac475de81bf27413a1d5
Hugo Roque <hugolnx@gmail.com>
2017-04-20 16:24:56 -0300

    Release 1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ContentGateway.Mixfile do
 
   def project do
     [app: :content_gateway,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
